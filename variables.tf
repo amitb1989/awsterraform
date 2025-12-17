3f918b84914f5015ff8984bea6d08e2af0d554c4
amitb1989 <amit.bapodara@hotmail.com>
2025-12-17 06:37:54 +0000

    delete output from main.tf

---
+++ variables.tf
@@ -0,0 +1,40 @@
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR for the public subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "my_ip_cidr" {
+  description = "Your IP in CIDR format to allow SSH (e.g., 203.0.113.10/32)"
+  type        = string
+}
+
+variable "allow_http_cidr" {
+  description = "CIDR range allowed to access HTTP port 8080"
+  type        = string
+  default     = "0.0.0.0/0"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "key_name" {
+  description = "Name of an existing AWS key pair to SSH into EC2"
+  type        = string
+}
