afad74e41d06204c55e191479ff2de9bad028d04
amitb1989 <amit.bapodara@hotmail.com>
2025-12-18 04:51:27 +0000

    added

--- modules/security_group/main.tf
+++ modules/security_group/main.tf
@@ -14,8 +14,8 @@ resource "aws_security_group" "this" {
 
   ingress {
     description = "HTTP HelloWorld on 8080"
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
