814cfbbabcc38dfa4ba8fae165a3a3c4e56938bc
amitb1989 <amit.bapodara@hotmail.com>
2025-12-18 03:56:39 +0000

    added

--- variables.tf
+++ variables.tf
@@ -1,27 +1,3 @@
-
-variable "region" {
-  description = "AWS region"
-  type        = string
-  default     = "ap-south-1"
-}
-
-variable "vpc_cidr" {
-  description = "CIDR for the VPC"
-  type        = string
-  default     = "10.0.0.0/16"
-}
-
-variable "public_subnet_cidr" {
-  description = "CIDR for the public subnet"
-  type        = string
-  default     = "10.0.1.0/24"
-}
-
-variable "my_ip_cidr" {
-  description = "Your IP in CIDR format to allow SSH (e.g., 203.0.113.10/32)"
-  type        = string
-}
-
 variable "allow_http_cidr" {
   description = "CIDR range allowed to access HTTP port 8080"
   type        = string
@@ -34,26 +10,12 @@ variable "instance_type" {
   default     = "t3.micro"
 }
 
-variable "key_name" {
-  description = "Name of an existing AWS key pair to SSH into EC2"
-  type        = string
-}
-
-
-variable "ami_id" {
-  description = "AMI ID to use for EC2 (approved in your org)"
-  type        = string
-}
-
 
 variable "region" {
-  default = "ap-south-1"
+  default = "us-east-1"
 }
 
 variable "subnet_id" {}
 variable "security_group_id" {}
-variable "instance_type" {
-  default = "t3.micro"
-}
 variable "key_name" {}
 variable "ami_id" {}
