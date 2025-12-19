6d863639a8f6f8bfe9f84fc0f9a4349b8d3805f8
amitb1989 <amit.bapodara@hotmail.com>
2025-12-19 06:12:07 +0000

    added

--- modules/security_group/main.tf
+++ modules/security_group/main.tf
@@ -19,7 +19,14 @@ resource "aws_security_group" "this" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+  
+ingress {
+    description = "HTTP 8080"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+}
   egress {
     description = "All egress"
     from_port   = 0
